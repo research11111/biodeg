9e3b0afe7542a0f79f680f81914aa775c6e46d8b
research11111 <urgent.research@mailo.fr>
2023-10-14 01:41:54 +0200

    fix paths

--- biodeg/biodeg.py
+++ biodeg/biodeg.py
@@ -8,13 +8,13 @@ def main():
              "Examples: " + \
              "\n" + \
              " Train a model:\n" + \
-             "   ./biodeg.py train -e 100 -i data/data/All-Public_dataset_Mordred.csv -o data/models/train_epoch100_all_dataset.pt\n" + \
+             "   biodeg train -e 100 -i data/data/All-Public_dataset_Mordred.csv -o data/models/train_epoch100_all_dataset.pt\n" + \
              "\n" + \
              " Test model accuracy:\n" + \
-             "   ./biodeg.py test -m data/models/train_epoch100_all_dataset.pt -i data/data/All-Public_dataset_Mordred.csv\n" + \
+             "   biodeg test -m data/models/train_epoch100_all_dataset.pt -i data/data/All-Public_dataset_Mordred.csv\n" + \
              "\n" + \
              " Make prediction on real samples:\n" + \
-             "   ./biodeg.py predict -i data/data/All-Public_dataset_Mordred_tail_10.csv -m data/models/train_epoch100_all_dataset.pt\n";
+             "   biodeg predict -i data/data/All-Public_dataset_Mordred_tail_10.csv -m data/models/train_epoch100_all_dataset.pt\n";
 
     def valid_file(param):
         if not os.path.exists(param):
@@ -32,17 +32,17 @@ def main():
 
     parser_train = subparsers.add_parser("train", help="Train model")
     parser_train.add_argument("-e", "--epoch", type=int, default=10, help="Number of epoch for the training step")
-    parser_train.add_argument("-o", "--output", type=str, default="data/models/model.pt", help="Output file for the model")
-    parser_train.add_argument("-i", "--input", default="data/data/All-Public_dataset_Mordred_10.csv", type=valid_file, help="Input file to train the model with")
+    parser_train.add_argument("-o", "--output", type=str, default="model.pt", help="Output file for the model")
+    parser_train.add_argument("-i", "--input", type=valid_file, help="Input file to train the model with")
     parser_train.add_argument("-t", "--test", action="store_true", default=False, help="Run tests after model training")
 
     parser_predict = subparsers.add_parser("predict", help="Predict biodegradability")
-    parser_predict.add_argument("-i", "--input", default="data/data/All-Public_dataset_Mordred_10.csv", type=valid_file, help="Input file to evaluate")
+    parser_predict.add_argument("-i", "--input", type=valid_file, help="Input file to evaluate")
     parser_predict.add_argument("-m", "--model", default=model_path("model.pt"), type=valid_file, help="Model to use to make the prediction")
     parser_predict.add_argument("-o", "--output", default="guessed.csv", type=str, help="Where to put the results")
 
     parser_predict = subparsers.add_parser("test", help="Test a model on some data")
-    parser_predict.add_argument("-i", "--input", default="data/data/All-Public_dataset_Mordred_10.csv", type=valid_file, help="Input file to evaluate")
+    parser_predict.add_argument("-i", "--input", type=valid_file, help="Input file to evaluate")
     parser_predict.add_argument("-m", "--model", default=model_path("model.pt"), type=valid_file, help="Model to use to make the prediction")
 
     args = parser.parse_args()
